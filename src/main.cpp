0067f1a0bf8f38f2c473b5ca1f9e9baac0eba433
coltersnyder <programmer114@gmail.com>
2023-04-10 21:42:22 -0600

    Made basic template

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include "scene.h"
+#include "scenerunner.h"
+
+#include <memory>
+
+static std::map< std::string, std::string > sceneData = {
+		{"basic", "Basic scene."}
+};
+
+int main(int argc, char *argv[])
+{
+	SceneRunner runner("Assignment 2 ", 500, 500);
+
+	std::unique_ptr<Scene> scene;
+
+	//scene = std::unique_ptr<Scene>(new SceneBasic()); // Add our main scene
+
+	return runner.run(std::move(scene));
+}
+
+
